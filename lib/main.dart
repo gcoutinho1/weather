c7d324a2e78d0d412048e7c9f6a118ca4f639475
Guilherme Coutinho <gcoutinho.dev@gmail.com>
2022-07-13 21:26:46 -0300

    http requests working correctly

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Weather',
       theme: ThemeData.dark(),
       home: LoadingScreen(),
     );
--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:weather/services/location.dart';
 
 class LoadingScreen extends StatefulWidget {
@@ -12,6 +15,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
   @override
   void initState() {
     getLocation();
+    getData();
     super.initState();
   }
 
@@ -31,7 +35,30 @@ class _LoadingScreenState extends State<LoadingScreen> {
   void getLocation() async {
     Location location = Location();
     await location.getCurrentLocation();
-    // print(location.latitude);
-    // print(location.longitude);
+    print(location.latitude);
+    print(location.longitude);
+  }
+
+  void getData() async {
+    // var url = Uri.parse('https://samples.openweathermap.org/data/2.5/weather');
+    http.Response response = await http.get(Uri.parse(
+        'https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22'));
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+
+      var longitude = jsonDecode(data)['coord']['lon'];
+      // print(longitude);
+
+      int weatherDescription = jsonDecode(data)['weather'][0]['id'];
+      double tempDescription = jsonDecode(data)['main']['temp'];
+      String cityDescription = jsonDecode(data)['name'];
+
+      print('cidade: $cityDescription');
+      print('temperatura: $tempDescription');
+      print('id: $weatherDescription');
+    } else {
+      print(response.statusCode);
+    }
   }
 }
